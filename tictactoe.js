e3c549899ebf249aec242aca82351e3b05afaece
Kanchan Chauhan <kanchan827@gmail.com>
2021-04-14 19:31:04 -0700

    set up play game logic

--- tictactoe.js
+++ tictactoe.js
@@ -9,39 +9,54 @@ const board = [
 ]
 
 const printBoard = (board) => {
+
   for (var row of board) {
     console.log(...row);
   }
+
 }
 
-const playGame = (player) => {
+const playTurn = (player) => {
+
   const num = prompt(`Enter a number player ${player} : `);
-  placePiece(num, board, player);
+
+  if(num < 1 || num > 9) {
+    prompt('Invalid input, please try again!');
+    playTurn(player)
+
+  } else {
+    placePiece(num, board, player);
+    
+    if(player === 'X') {
+      playTurn('O');
+    } else {
+      playTurn('X');
+    }
+  }
 }
 
 const placePiece = (num, board, player) => {
 
     if(num === '1') {
-      console.log('here');
-      board[0][0] = 'X';
+      board[0][0] = player;
     } else if (num === '2') {
-      board[0][2] = 'X';
+      board[0][2] = player;
     } else if (num === '3') {
-      board[0][4] = 'X';
+      board[0][4] = player;
     } else if (num === '4') {
-      board[2][0] = 'X';
+      board[2][0] = player;
     } else if (num === '5') {
-      board[2][2] = 'X';
+      board[2][2] = player;
     } else if (num === '6') {
-      board[2][4] = 'X';
+      board[2][4] = player;
     } else if (num === '7') {
-      board[4][0] = 'X';
+      board[4][0] = player;
     } else if (num === '8') {
-      board[4][2] = 'X';
+      board[4][2] = player;
     } else if (num === '9') {
-      board[4][4] = 'X';
+      board[4][4] = player;
     }
   printBoard(board);
 }
 
-playGame('X');
+playTurn('X');
