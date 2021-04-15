a1ea2004a02531106689bf1a629e2269d424d5d1
Kanchan Chauhan <kanchan827@gmail.com>
2021-04-14 19:18:04 -0700

    print board

--- tictactoe.js
+++ tictactoe.js
@@ -1,4 +1,4 @@
-const prompt = require('prompt');
+const prompt = require('prompt-sync')({sigint: true});
 
 const board = [
   [' ', '|', ' ', '|', ' '],
@@ -13,4 +13,35 @@ const printBoard = (board) => {
     console.log(...row);
   }
 }
-printBoard(board);
+
+const playGame = (player) => {
+  const num = prompt(`Enter a number player ${player} : `);
+  placePiece(num, board, player);
+}
+
+const placePiece = (num, board, player) => {
+
+    if(num === '1') {
+      console.log('here');
+      board[0][0] = 'X';
+    } else if (num === '2') {
+      board[0][2] = 'X';
+    } else if (num === '3') {
+      board[0][4] = 'X';
+    } else if (num === '4') {
+      board[2][0] = 'X';
+    } else if (num === '5') {
+      board[2][2] = 'X';
+    } else if (num === '6') {
+      board[2][4] = 'X';
+    } else if (num === '7') {
+      board[4][0] = 'X';
+    } else if (num === '8') {
+      board[4][2] = 'X';
+    } else if (num === '9') {
+      board[4][4] = 'X';
+    }
+  printBoard(board);
+}
+
+playGame('X');
