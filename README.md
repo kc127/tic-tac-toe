baedc3a4378f39d1f928bd0c8a92c1748fb1a23c
Kanchan Chauhan <kanchan827@gmail.com>
2021-04-14 21:44:02 -0700

    bare mins complete

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # tic-tac-toe
+
+Make a command line tic-tac-toe game from scratch for two players.
+
+Expected features
+===============
+* Minimal UI that redraws the board and makes clear whose turn it is, each turn.
+* Players can submit moves (assume, admittedly unrealistically, that both players are sitting at the same keyboard).
+* Win detection - detect and display who won
+
+Bonus / stretch goals (any or all of the following)
+=======================================
+* Structure your code such that the UI can be turned easily into a native mobile app (iOS say) without having to rewrite the core game logic.
+* Implement win detection with a functional rather than iterative style.
+* Between moves, rotate the board 90 degrees counter-clockwise. The moves "fall" due to "gravity", post-rotation.
+
+Implementation instructions
+=======================
+* Create the project from scratch. Don't just clone an existing project.
+* This includes writing configuration files for any dependencies and test framework setup.
+* You should have a reasonably thorough suite of unit tests using a real unit test framework.
+* Use the editor of your choice.
+* Init a git repo for this project.
+* Push the repo up to github.
+* Make small commits as you go to illustrate your thought process and be able to back out changes easily.
+* Don't forget to push your final solution up to Github.
+* Add a professional-looking README file with installation and usage instructions.
--- tictactoe.js
+++ tictactoe.js
@@ -1,4 +1,4 @@
-const prompt = require('prompt-sync')({sigint: true});
+const prompt = require('prompt-sync')({ sigint: true });
 
 const board = [
   [' ', '|', ' ', '|', ' '],
@@ -8,55 +8,92 @@ const board = [
   [' ', '|', ' ', '|', ' ']
 ]
 
-const printBoard = (board) => {
+const winningCombinations = [[1, 2, 3], [4, 5, 6], [7, 8, 9],
+                             [1, 4, 7], [2, 5, 8], [3, 6, 9]];
 
+const playerXPositions = [];
+const playerOPositions = [];
+
+
+const printBoard = (board) => {
   for (var row of board) {
     console.log(...row);
   }
-
 }
 
-const playTurn = (player) => {
+const playGame = (player) => {
 
   const num = prompt(`Enter a number player ${player} : `);
 
-  if(num < 1 || num > 9) {
+  if (num < 1 || num > 9) {
     prompt('Invalid input, please try again!');
     playTurn(player)
-
+  } else if (playerXPositions.includes(parseInt(num)) || playerOPositions.includes(parseInt(num))){
+    prompt('Cell has already been chose, please try again!');
+    playGame(player);
   } else {
+
     placePiece(num, board, player);
-    
-    if(player === 'X') {
-      playTurn('O');
+
+    if (player === 'X') {
+      playerXPositions.push(parseInt(num));
     } else {
-      playTurn('X');
+      playerOPositions.push(parseInt(num));
+    }
+    console.log(playerXPositions, playerOPositions);
+
+    const playerPosition = playerXPositions.length === 3 ? playerXPositions : playerOPositions.length === 3 ? playerOPositions : null ;
+
+    if (playerPosition && checkWinner(playerPosition) === true) {
+      console.log('Congratulations! You won!')
+      return;
+    }
+    if (player === 'X') {
+      playGame('O');
+    } else {
+      playGame('X');
     }
   }
+}
+
+const checkWinner = (playerPosition) => {
+
+  let current, count;
+  for (let i = 0; i < winningCombinations.length; i++) {
+    current = winningCombinations[i];
+    count = 0
+    for (let j = 0; j < playerPosition.length && playerPosition[j] === current[j]; j++ ) {
+      count++;
+      if(count === 3) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 const placePiece = (num, board, player) => {
 
-    if(num === '1') {
-      board[0][0] = player;
-    } else if (num === '2') {
-      board[0][2] = player;
-    } else if (num === '3') {
-      board[0][4] = player;
-    } else if (num === '4') {
-      board[2][0] = player;
-    } else if (num === '5') {
-      board[2][2] = player;
-    } else if (num === '6') {
-      board[2][4] = player;
-    } else if (num === '7') {
-      board[4][0] = player;
-    } else if (num === '8') {
-      board[4][2] = player;
-    } else if (num === '9') {
-      board[4][4] = player;
-    }
+  if (num === '1') {
+    board[0][0] = player;
+  } else if (num === '2') {
+    board[0][2] = player;
+  } else if (num === '3') {
+    board[0][4] = player;
+  } else if (num === '4') {
+    board[2][0] = player;
+  } else if (num === '5') {
+    board[2][2] = player;
+  } else if (num === '6') {
+    board[2][4] = player;
+  } else if (num === '7') {
+    board[4][0] = player;
+  } else if (num === '8') {
+    board[4][2] = player;
+  } else if (num === '9') {
+    board[4][4] = player;
+  }
   printBoard(board);
 }
 
-playTurn('X');
+playGame('X');
